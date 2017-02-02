e91f10022dd894a12213e74112769c937a37bf2b
shyclown <roman.moravcik1@gmail.com>
2017-02-02 09:26:36 +0100

    Session and Account

--- system/php/class_account.php
+++ system/php/class_account.php
@@ -2,17 +2,23 @@
 
 class Account
 {
+  private $id;
+  private $username;
+  private $email;
+  private $password;
 
   function __construct(){
     $this->db = new Database;
     $this->error = [];
-    // create tables
+    // create tables if not exists
     $this->create_table_account();
   }
+  /* Create Table */
   private function create_table_account(){
-    $sql = "CREATE TABLE IF NOT EXISTS `ml_article` (
+    $sql = "CREATE TABLE IF NOT EXISTS `ml_account` (
             `id` int(8) NOT NULL AUTO_INCREMENT,
-            `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+            `username` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+            `email` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
             `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
             `state` int(1) NOT NULL,
             `date_created` datetime NOT NULL,
@@ -22,95 +28,181 @@ class Account
           DEFAULT CHARSET=utf8 COLLATE=utf8_bin";
     $this->db->query($sql);
   }
-  public function createAccount($data){
-    // create account
-    $hash_password = $hash($data['password']);
-    $email = $data['email'];
 
-  }
-  public function search($data){
-    $sql = "SELECT * FROM `ml_article` WHERE `header` LIKE CONCAT('%',?,'%')";
-    $params = array('s',$data['search']);
-    return $this->db->query($sql, $params);
+  // public: SIGN IN, LOGIN, DELETE
+
+  public function sign_in(){
+    $this->load_post_values();
+    $this->check_values();
+    if(empty($this->errors)){ $this->create_account(); echo 'new account created';}
+    else{ var_dump($this->errors); }
   }
 
-  public function load_files($data){
-    $sql = "SELECT f.*, af.file_desc
-            FROM `ml_article_file` af
-            INNER JOIN `ml_file` f
-            ON af.file_id = f.id WHERE af.article_id = ?";
-    $params = array('i',$data['id']);
-    return $this->db->query($sql,$params);
-  }
-  public function number_of_rows(){
-    $sql = 'SELECT COUNT(*) AS `count` FROM `ml_article`';
-    return $this->db->query($sql);
-  }
-  /* Select ALL Articles */
-  public function select_all($data){
-    $sql = "SELECT id, header FROM `ml_article` ORDER BY id DESC";
-    return $this->db->query($sql);
-  }
-  /* Select articles IN FOLDER */
-  public function select($data){
-    $order = 'DESC';
-    $sort_by = 'id';
-    if(isset($data['order'])){
-      if($data['order']){ $order = 'ASC'; }
-      else { $order = 'DESC';}
+  public function login(){
+    // logname can be email or username
+  $this->logname = $_POST['logname'];
+  $this->password = $_POST['password'];
+    if($this->find_account()){ $this->make_session();
+    //  $this->make_cookie();
     }
-    if(isset($data['sort_by'])){
-      if($data['sort_by'] == 'name'){ $sort_by = 'header'; }
-      if($data['sort_by'] == 'date'){ $sort_by = 'date_created'; }
-      //if($data['sort_by'] == 'size'){ $sort_by = 'file_size'; }
+    else{ echo "not valid username or password"; }
+  }
+
+  public function delete(){
+  $this->user_id = $_SESSION['user_id'];
+  $sql = "DELETE FROM `ml_account` WHERE id = ?";
+  $params = array('i', $this->user_id);
+  $result = $db->query($sql, $params);
+  }
+
+  public function load_signed(){
+  if(isset($_SESSION['user_id'])){
+    $id = $_SESSION['user_id'];
+    $sql = "SELECT * FROM `el_account` WHERE `id` = ?";
+    $params = array("i",$id);
+    $result = $this->db->query($sql,$params);
+    if(!empty($result)){
+      $this->id = $result[0]['id'];
+      $this->username = $result[0]['username'];
+      $this->email = $result[0]['email'];
+      }
     }
-    $sql = "SELECT SQL_CALC_FOUND_ROWS a.* FROM `ml_article` a
-            INNER JOIN `ml_article_folder` af ON a.id = af.article_id
-            WHERE af.folder_id = ?
-            ORDER BY ".$sort_by." ".$order." LIMIT ? , ?";
-    $sql_all_rows = "SELECT FOUND_ROWS()";
-    $params = array('iii',$data['folder_id'],$data['limit_min'],$data['limit_max']);
+    else { array_push($this->errors, 'Account does not exists'); }
+  }
+  private function load_post_values(){
+    $this->username = $_POST['username'];
+    $this->email = $_POST['email'];
+    $this->password = $this->generate_hash($_POST['password']);
+    // $this->salt = $_POST['password']; testing
+  }
+
+  /* Check values */
+
+  public function check_email(){
+    if(isset($_POST) && $_POST != ''){
+      $this->email = $_POST['email'];
+      if($this->is_valid('email')){
+        if($this->is_free('email')){
+          echo 'email is ok';
+        }
+        else{ echo 'email is not free'; }
+      }
+      else{
+        echo 'not valid email';
+      }
+    }
+  }
+
+  public function is_valid($column)
+  {
+    if($column == 'email'){ return filter_var(  $this->email, FILTER_VALIDATE_EMAIL); }
+    if($column == 'username'){ return preg_match('/^[A-Za-z][A-Za-z\d_.-]{5,31}$/i', $this->username); }
+  }
+
+  public function is_free($column)
+  {
+    if($column == 'email'){ $value = $this->email; }
+    if($column == 'username'){ $value = $this->username; }
+    $sql = "SELECT * FROM `el_account` WHERE ? = ?";
+    $params = array('ss', $column, $value);
     $result = $this->db->query($sql, $params);
-    $all = $this->db->query($sql_all_rows);
-    return array('result' => $result, 'all_rows'=> $all[0]['FOUND_ROWS()']);
+    return $result === NULL;
   }
-  public function select_by_id($data){
-    $sql = "SELECT * FROM `ml_article` WHERE id = ?";
-    $params = array('i',$data['id']);
-    return $this->db->query($sql,$params);
+
+  public function list_all()
+  {
+    $sql = "SELECT * FROM `el_account`";
+    $result = $this->db->select($sql);
+    return $result;
   }
-  public function insert($data){
-    $sql = "INSERT INTO `ml_article` (`id`, `header`, `content`, `state`, `date_created`, `date_edited`)
-            VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
-    $params = array( 'ssi', $data['header'], $data['content'], $data['state'] );
-    // returns ID of inserted draft
-    $article_id = $this->db->query($sql, $params , 'get_id');
-    $this->add_to_folder($article_id, $data['folder_id']);
-    return $article_id;
+
+  private function valid_password($input, $hashed)
+  {
+    return crypt($input, $hashed) == $hashed;
   }
-  public function add_to_folder($article_id, $folder_id){
-    $sql = "INSERT INTO `ml_article_folder` (`id`,`article_id`, `folder_id`)
-            VALUES (NULL, ?, ?)";
-    $params = array( 'ii', $article_id, $folder_id );
-    $this->db->query($sql, $params);
+
+  private function generate_hash($password, $cost = 11)
+  {
+    $salt= substr(base64_encode(openssl_random_pseudo_bytes(17)),0,22);
+    $salt= str_replace("+",".",$salt);
+    $param='$'.implode('$',array("2y", str_pad($cost,2,"0",STR_PAD_LEFT), $salt));
+    return crypt($password,$param);
   }
-  public function update($data){
-    $sql = "UPDATE `ml_article` SET `header`=?, `content`=?, `state`=?, `date_edited`=CURRENT_TIMESTAMP  WHERE `id` = ?";
-    $params = array('ssii', $data['header'], $data['content'], $data['state'], $data['id']);
-    return $this->db->query($sql,$params);
+
+  private function check_values()
+  {
+    if(!isset($this->email) || $this->email == ''){
+      array_push($this->errors,'email not set');}
+    if(!$this->is_valid('email')){
+      array_push($this->errors,'email writen wrong');}
+    if(!$this->is_free('email')){
+      array_push($this->errors,'email already used by someone else');}
+
+    if(!isset($this->username) || $this->username == ''){
+      array_push($this->errors,'username not set');}
+    if(!$this->is_valid('username')){
+      array_push($this->errors,'username is not valid');}
+    if(!$this->is_free('username')){
+      array_push($this->errors,'username already used by someone else');}
   }
-  public function delete($data){
-    $params = array('i',$data['id']);
-    $sql_article = "DELETE FROM `ml_article`
-                    WHERE `ml_article`.`id` = ?";
-    $sql_article_file = "DELETE FROM `ml_article_file`
-                          WHERE `ml_article_file`.`article_id` = ?";
-    $sql_article_folder = "DELETE FROM `ml_article_folder`
-                          WHERE `ml_article_folder`.`article_id` = ?";
-    $article = $this->db->query($sql_article,$params);
-    $file = $this->db->query($sql_article_file,$params);
-    $folder = $this->db->query($sql_article_folder,$params);
-    return $article && $file && $folder;
+
+  private function create_account()
+  {
+    $sql = "INSERT INTO `el_account` (`id`, `username`, `password`, `email`, `salt`) VALUES (NULL, ?, ?, ?, ?);";
+    $params = array('ssss', $this->username , $this->password , $this->email, $this->salt);
+    $result = $this->db->query($sql, $params);
+    echo 'user created';
   }
+
+  private function find_account()
+  {
+    $sql = "SELECT * FROM `el_account` WHERE `username` = ? OR `email` = ?";
+    $params = array("ss", $this->logname ,$this->logname);
+    $result = $this->db->query($sql, $params);
+    if(!empty($result))
+    {
+      if($this->valid_password($this->password, $result[0]['user_pass']))
+      {
+        $this->id = $result[0]['id'];
+        $this->username = $result[0]['username'];
+        $this->email = $result[0]['email'];
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+
+  private function make_cookie()
+  {
+    if(!setcookie('elephant-id',session_id(),strtotime( '+30 days' ), "/", NULL)){
+      echo 'cookie not made';
+    };
+  }
+
+  private function make_session()
+  {
+    $_SESSION["user_id"] = $this->id;
+    $_SESSION["username"] = $this->username;
+    $_SESSION["loged_in"] = true;
+  }
+
+  public function is_valid_cookie()
+  {
+    $sql = "SELECT * FROM `ml_sessions` WHERE `id` = ? LIMIT 1";
+    $params = array('s', $_COOKIE['missionlife-id']);
+    $result = $this->db->query($sql, $params);
+    $data = $result[0]['data'];
+    if(session_decode ( $data ))
+    {
+    return true;
+    }
+    return false;
+  }
+
+
+
 }
 ?>
