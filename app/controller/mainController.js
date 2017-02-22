6ba0dbcc9b3631638c66ec1de6ffd6149898b244
shyclown <roman.moravcik1@gmail.com>
2017-02-22 14:11:13 +0100

    change

--- app/controller/mainController.js
+++ app/controller/mainController.js
@@ -3,23 +3,25 @@ app.controller('mainController',function($http, Data , $scope, $route, $routePar
   $scope.page = {};
   /* Page Data */
   Data.select({ name:'settings' },function(res){
-    res.data[0].data = JSON.parse(res.data[0].data);
-    $scope.settings = res.data[0];
-
-    if(!$scope.settings){
-      // Set drfault values
+    console.log(res);
+    if(!res.data){
+      console.log('epty');
       $scope.settings = {
-        name: 'settings',
-        data:{
-        page_name: 'Default page Name',
-        page_motto: 'Default under Name'
-        }
-      }
-      Data.insert( $scope.settings , function(res){
-        $scope.settings.id = res.data;
-        $scope.page = $scope.settings.data;
-      });
-    }else{ $scope.page = $scope.settings.data; }
+            name: 'settings',
+            data:{
+            page_name: 'Default page Name',
+            page_motto: 'Default under Name'
+            }
+          }
+          Data.insert( $scope.settings , function(res){
+            $scope.settings.id = res.data;
+            $scope.page = $scope.settings.data;
+          });
+    }else{
+      res.data[0].data = JSON.parse(res.data[0].data);
+      $scope.settings = res.data[0];
+    }
+    $scope.page = $scope.settings.data;
   });
 
   /* Route Info */
