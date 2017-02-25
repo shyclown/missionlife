0efefc281799cf23a4dc0595ba682bb608321f9a
shyclown <roman.moravcik1@gmail.com>
2017-02-25 17:47:09 +0100

    update

--- app/directive/explorer/pop_explorer.js
+++ app/directive/explorer/pop_explorer.js
@@ -1,4 +1,5 @@
-app.directive('popFolderExplorer',['$http','Shared', 'Folder', 'Article','FileService','uploadDropped', function($http, Shared, Folder, Article, FileService, uploadDropped) {
+app.directive('popFolderExplorer',['$http','Shared', 'Folder', 'Article','FileService','uploadDropped',
+function($http, Shared, Folder, Article, FileService, uploadDropped) {
   return {
     restrict: 'E',
     scope:{
@@ -6,7 +7,56 @@ app.directive('popFolderExplorer',['$http','Shared', 'Folder', 'Article','FileSe
     templateUrl: '/missionlife/app/template/pop_folder_window.html',
     link: function (scope, element, attrs)
     {
+
       scope.currentFolder = Shared.currentFolder;
+      scope.folders = [];
+      scope.articles = [];
+      scope.files = [];
+      scope.currentParents = [];
+      scope.openFoldersInTree = [];
+      // Article and File can be changed updated in outside directive
+      scope.$watch( function(){ return Article.selected; }, function(){ scope.articles = Article.selected;}, true);
+      scope.$watch( function(){ return FileService.selected; }, function(){ scope.files = FileService.selected;}, true);
+      scope.$watch( function(){ return Folder.allFolders; }, function(){ scope.folders = Folder.allFolders;}, true);
+      // Load Folders
+      Folder.select_all();
+      const stopDefault = function(){ event.stopPropagation(); event.preventDefault(); }
+
+
+      /* Folder Editor Window */
+
+      scope.folderWindow = false;
+      scope.editFolder = {};
+      scope.toogleEditFolder = function(){ scope.folderWindow = !scope.folderWindow; }
+      scope.setEditFolder = function(folder){
+        stopDefault(event);
+        scope.editFolder = folder;
+        scope.toogleEditFolder();
+      }
+      scope.setPosition = function(position){
+        scope.editFolder.position = position;
+        scope.updatePosition(scope.editFolder);
+      }
+
+      scope.new_folder = {};
+      scope.saveNewFolder = function(){
+        let data = scope.new_folder;
+        data.parent = scope.currentFolder;
+        Folder.insert(data, function(response){
+          scope.new_folder.name = "";
+        });
+      }
+
+      /* Form Window Data */
+
+      scope.formWindow = false;
+      scope.openForm = false;
+      scope.openFormWindow = function(){
+        scope.formWindow = true;
+      }
+
+      /* Article Window Data */
+
       scope.articleWindow = false;
       scope.openArticle = {
         new: true,
@@ -16,88 +66,11 @@ app.directive('popFolderExplorer',['$http','Shared', 'Folder', 'Article','FileSe
         console.log(scope.openArticle);
       scope.newArticle = {};
 
-      const callbackFn = scope.$eval(attrs.callbackFn);
-      const popSetup = {
-        articles: false,
-        article_click: false,
-        files: false,
-        file_click: false,
-        submit: true
-      }
-
-      scope.formWindow = false;
-      scope.openForm = false;
-      scope.openFormWindow = function(){
-        console.log('open');
-        scope.formWindow = true;
-      }
-
-      // Watch Folders
-      scope.folders;
-      scope.currentParents = [];
-      scope.articles = [];
-      scope.openFoldersInTree = [];
-      /*
-      Article and File can be changed updated in outside directive
-      */
-      scope.$watch( function(){ return Article.selected; }, function(){ scope.articles = Article.selected;}, true);
-      scope.$watch( function(){ return FileService.selected; }, function(){ scope.files = FileService.selected;}, true);
-      scope.$watch( function(){ return Folder.allFolders; }, function(){ scope.folders = Folder.allFolders;}, true);
-      // Load Folders
-      Folder.select_all();
-      // Load Articles in Folder
-
-      const loadArticles = function(){
-        Article.selectByFolder({ folder_id: scope.currentFolder.id });
-      }
-
-      const afterUploadOne = function(response, update){
-        FileService.attachToFolder({
-          file_id: response.data.file_id,
-          folder_id: scope.currentFolder.id },
-          function(res){ if(update){ afterUploadAll(); }
-        });
-      }
-      const afterUploadAll = function(response){ FileService.refresh(); }
-
-      scope.uploadFile = function(){
-        const files = event.dataTransfer.files;
-        const update = function(){}
-        let completed = 0;
-        let all = files.length;
-
-        for (let i = 0; i < all; i++){
-          let file = [files[i]];
-          let res = uploadDropped.bind(null, file,
-            function(response){},
-            function(response){
-              completed++;
-              let update = completed == all;
-              afterUploadOne(response, update);
-            })();
-        }
-
-      }
-
-      const stopDefault = function(){
-        event.stopPropagation();
-        event.preventDefault();
-      }
-      const inFolderArray = function(id){
-        let pos = scope.openFoldersInTree.indexOf(id);
-        return {  open: pos >= 0, position: pos };
-      }
-      // ISSUE: After changes when article is created it jumps to parent Folder but saves Article to right folder
-
       scope.selectArticle = function(article){
         scope.articleWindow = true;
         scope.openArticle = article;
       }
 
-
-      scope.callbackArticleWindow = function(action){
-
-      }
       scope.createNewArticle = function(){
         Article.insert({
           header: 'New Article',
@@ -112,11 +85,40 @@ app.directive('popFolderExplorer',['$http','Shared', 'Folder', 'Article','FileSe
           });
         })
       }
-      // disable placing articles to null folder
-      scope.isOpenFolder = function(){ return scope.currentFolder != null; }
 
+      /* File Upload On Drop */
+
+      scope.uploadFile = function(){
+        const files = event.dataTransfer.files;
+        const all = files.length;
+        const oData = function(response){
+          return { file_id: response.data.file_id, folder_id: scope.currentFolder.id }
+        }
+        const refreshFiles = function(update){
+          const fn = function(){ if(update){ FileService.refresh(function(){ scope.$apply(); }); } }
+          return fn;
+        }
+        // refres only after all filea are uploaded
+        let completed = 0;
+        for (let i = 0; i < all; i++){ let file = [files[i]];
+          uploadDropped(file, false, function(response){
+            let update = completed == all;
+            FileService.attachToFolder( oData(response), refreshFiles(update));});
+        }
+      }
+
+      /* Folders */
+
+      scope.isOpenFolder = function(){ return scope.currentFolder != null; }
       scope.openFolder = function(folder){
-        if(folder == null){ scope.currentFolder = null; }
+        const inFolderArray = function(id){
+          let pos = scope.openFoldersInTree.indexOf(id);
+          return {  open: pos >= 0, position: pos };
+        }
+        if(folder == null){
+          scope.currentFolder = folder;
+          Shared.currentFolder = folder;
+        }
         else{
           const folderInArray = inFolderArray(folder.id);
           const folderIsCurrent = folder == scope.currentFolder;
@@ -131,52 +133,14 @@ app.directive('popFolderExplorer',['$http','Shared', 'Folder', 'Article','FileSe
           if(!folderInArray.open){ scope.openFoldersInTree.push(folder.id); }
         }
       }
-
-      /* Folder Editor Window */
-      scope.folderWindow = false;
-      scope.editFolder = {};
-      scope.toogleEditFolder = function(){ scope.folderWindow = !scope.folderWindow; }
-      scope.setEditFolder = function(folder){
-        stopDefault(event);
-        scope.editFolder = folder;
-        scope.toogleEditFolder();
-      }
-      scope.setPosition = function(position){
-        scope.editFolder.position = position;
-        scope.updatePosition(scope.editFolder);
-      }
-
-      scope.isOpen = function(folder){
-        return folder.id == scope.currentFolder.id;
-      }
-      // New Folder Object
-      scope.new_folder = {};
-      scope.saveNewFolder = function(){
-        let data = scope.new_folder;
-        data.parent = scope.currentFolder;
-        Folder.insert(data, function(response){
-          scope.new_folder.name = "";
-        });
-      }
-      scope.removeFolder = function(folder){ stopDefault(event);
-        Folder.remove(folder);
-      }
-      scope.orderUp = function(folder){ stopDefault(event);
-        Folder.orderUp(folder);
-      }
-      scope.orderDown = function(folder){ stopDefault(event);
-        Folder.orderDown(folder);
-      }
-      scope.updateName = function(folder){ stopDefault(event);
-        Folder.updateName(folder);
-      }
-      scope.updatePosition = function(folder){ stopDefault(event);
-        Folder.updatePosition(folder);
-      }
+      scope.isOpen = function(folder){ return folder.id == scope.currentFolder.id; }
+      scope.removeFolder = function(folder){ stopDefault(event); Folder.remove(folder); }
+      scope.orderUp = function(folder){ stopDefault(event); Folder.orderUp(folder); }
+      scope.orderDown = function(folder){ stopDefault(event); Folder.orderDown(folder); }
+      scope.updateName = function(folder){ stopDefault(event); Folder.updateName(folder); }
+      scope.updatePosition = function(folder){ stopDefault(event); Folder.updatePosition(folder); }
       // CSS Style
-      scope.currentOpen = function(folder){
-        return (folder.id == scope.openFolder.id) ? 'currentFolder' : '';
-      }
+      scope.currentOpen = function(folder){ return (folder.id == scope.openFolder.id) ? 'currentFolder' : ''; }
     }
   };
 }]);
