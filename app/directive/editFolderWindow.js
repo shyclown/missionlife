1f670707dad67672f9d80d5bb113a38629c04656
shyclown <roman.moravcik1@gmail.com>
2017-03-01 16:54:45 +0100

    update

--- app/directive/editFolderWindow.js
+++ app/directive/editFolderWindow.js
@@ -1,7 +1,23 @@
-app.directive('editFolderWindow', function() {
+app.directive('editFolderWindow', ['Folder', function(Folder) {
   return {
     restrict: 'E',
+    scope:{ folderWindow : '=editObj' },
     templateUrl: '/missionlife/app/template/edit_folder_window.html',
-    link: function (scope, element, attrs){}
+    link: function (scope, element, attrs){
+      const stopDefault = function(){ event.stopPropagation(); event.preventDefault(); }
+      scope.folder = scope.folderWindow.item;
+
+      scope.removeFolder = function(folder){ stopDefault(event); Folder.remove(folder); }
+      scope.orderUp = function(folder){ stopDefault(event); Folder.orderUp(folder); }
+      scope.orderDown = function(folder){ stopDefault(event); Folder.orderDown(folder); }
+      scope.updateName = function(folder){ stopDefault(event); Folder.updateName(folder); }
+      scope.updatePosition = function(folder){ stopDefault(event); Folder.updatePosition(folder); }
+
+      scope.close = function(){
+        scope.folderWindow.close();
+      }
+
+
+    }
   }
-});
+}]);
