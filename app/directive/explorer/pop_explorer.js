1a971bb1186f7456697d775922df06e02efdcc35
shyclown <roman.moravcik1@gmail.com>
2017-03-15 19:02:20 +0100

    changes in directive element creation

--- app/directive/explorer/pop_explorer.js
+++ app/directive/explorer/pop_explorer.js
@@ -17,8 +17,10 @@ function($http, Form, Shared, Folder, Article, Garant, FileService, uploadDroppe
 
       // Article and File can be changed updated in outside directive
 
-      scope.$watch( function(){ return Folder.allFolders; },
-      function(){ scope.folders = Folder.allFolders;}, true);
+      scope.$watch(
+        function(){ return Folder.allFolders; },
+        function(){ scope.folders = Folder.allFolders;},
+      true);
       // Load Folders
       Folder.select_all();
 
@@ -37,28 +39,36 @@ function($http, Form, Shared, Folder, Article, Garant, FileService, uploadDroppe
 
 
 
-      /* Window */
-      scope.openWindows = [];
-      const openWindow = function(name, item, callback){
-        console.log(item);
-        name = 'edit-'+name+'-window';
-        return  new Shared.directiveElement(name, item, callback, scope);
+      // Create Windows
+      scope.openFileWindow = function(file){
+        new Shared.directiveElement('edit-file-window', file, function(){
+          //callback
+        }, scope);
       }
-      /*
-      * Callbacks are executed after popup is confirmed
-      */
-      const callbackArticle = function(){}
-      const callbackFile = function(){}
-      const callbackFolder = function(){}
-      const callbackForm = function(){ Form.updateExplorer(); }
-      const callbackGarant = function(image){ if(image){ FileService.updateExplorer(); }; Garant.updateExplorer(); }
-
-      // Create Windows to Edit Items
-      scope.openFileWindow = function(file){ element.append(openWindow('file', file, callbackFile).el); }
-      scope.openFormWindow = function(form){ element.append(openWindow('form', form, callbackForm).el); }
-      scope.openFolderWindow = function(folder){ element.append(openWindow('folder', folder, callbackFolder).el); }
-      scope.openGarantWindow = function(garant){ element.append(openWindow('garant', garant, callbackGarant).el); }
-      scope.openArticleWindow = function(article){ element.append(openWindow('article', article, callbackArticle).el); }
+      scope.openFormWindow = function(form){
+        new Shared.directiveElement('edit-form-window', form, function(){
+          //callback
+          Form.updateExplorer();
+        }, scope);
+      }
+      scope.openFolderWindow = function(folder){
+        new Shared.directiveElement('edit-folder-window', folder, function(){
+          //callback
+        }, scope);
+      }
+      scope.openGarantWindow = function(garant){
+        new Shared.directiveElement('edit-garant-window', garant, function(image){
+          //callback
+          if(image){ FileService.updateExplorer(); };
+          Garant.updateExplorer();
+        }, scope);
+      }
+      scope.openArticleWindow = function(article){
+        new Shared.directiveElement('edit-article-window', article, function(){
+          //callback
+          Article.updateExplorer();
+        }, scope);
+      }
 
 
       scope.prompted = function(message, description, cancelBtn, acceptBtn, callback){
@@ -70,52 +80,39 @@ function($http, Form, Shared, Folder, Article, Garant, FileService, uploadDroppe
         }, callback, scope);
       }
 
-      scope.createNewArticle = function(){
-        const newArticle = { header: 'New Article', content: '<p>Content</p>', folder_id: scope.currentFolder.id, state: 0 }
-        Article.insert(newArticle, function( response ){
-          Article.select_by_id({id: response.data}, function(res){
-            element.append(openWindow('article', res.data[0], callbackArticle).el);
-          });
-        })
-      }
-
       /* File Upload On Drop */
       scope.uploadFile = FileService.uploadFile;
 
       scope.isOpenFolder = function(){ return scope.currentFolder != null; }
       scope.openFolder = function(folder){
         // prompted action
-        scope.prompted(
-          'Opening Folder',
-          'by submiting to this you are going to open folder',
-          'cancel',
-          'Open Folder',
-          function(){
-            const inFolderArray = function(id){
-              let pos = scope.openFoldersInTree.indexOf(id);
-              return {  open: pos >= 0, position: pos };
-            }
-            if(folder == null){
-              scope.currentFolder = folder;
-              Shared.explorer.current_folder = folder;
-            }
-            else{
-              const folderInArray = inFolderArray(folder.id);
-              const folderIsCurrent = folder == scope.currentFolder;
-              if(!folderIsCurrent){
-                Shared.explorer.current_folder = folder;
-                scope.currentFolder = folder;
-                scope.currentParents = Folder.listParents(folder);
-              }
-              if(folderInArray.open && folderIsCurrent){
-                scope.openFoldersInTree.splice(folderInArray.position, 1);
-              }
-              if(!folderInArray.open){
-                scope.openFoldersInTree.push(folder.id);
-              }
-            }
+        const inFolderArray = function(id)
+        {
+          let pos = scope.openFoldersInTree.indexOf(id);
+          return {
+            open: pos >= 0, position: pos
+          };
+        }
+        if(folder == null){
+          scope.currentFolder = folder;
+          Shared.explorer.current_folder = folder;
+        }
+        else{
+          const folderInArray = inFolderArray(folder.id);
+          const folderIsCurrent = folder == scope.currentFolder;
+
+          if(!folderIsCurrent){
+            Shared.explorer.current_folder = folder;
+            scope.currentFolder = folder;
+            scope.currentParents = Folder.listParents(folder);
           }
-        );
+          if(folderInArray.open && folderIsCurrent){
+            scope.openFoldersInTree.splice(folderInArray.position, 1);
+          }
+          if(!folderInArray.open){
+            scope.openFoldersInTree.push(folder.id);
+          }
+        }
       }
       scope.isOpen = function(folder){ return folder.id == scope.currentFolder.id; }
 
