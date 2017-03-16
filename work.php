f3409dfadb3468f0f91ed431f9edf9ebac36ec43
shyclown <roman.moravcik1@gmail.com>
2017-03-16 12:19:31 +0100

    placeholder page

---
+++ work.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Missionlife SK</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,600,700&amp;subset=latin-ext" rel="stylesheet">
+    <style>
+    @media query {
+
+    }
+    html,body{
+      width: 100%;
+      height: 100%;
+      padding: 0px;
+      margin: 0px;
+      font-family: 'Source Sans Pro', sans-serif;
+      font-weight: 200;
+      background-color: #e0e0e0;
+    }
+    .full{
+      box-sizing: border-box;
+      height: 100%;
+      width: 100%;
+
+    }
+    .p16{
+      padding: 16px;
+    }
+    .flex{ display: flex; }
+    .center{
+      justify-content: center;
+      align-items: center;
+    }
+
+    h1{
+      font-family: 'Source Sans Pro', sans-serif;
+      font-weight: 200;
+    }
+    .outlined{
+            background-color: #FFF;
+      outline: 1px #09F solid;
+    }
+    </style>
+
+  </head>
+  <body>
+
+    <div class="full center flex">
+      <div class="outlined p16">
+      <h1>Na stranke prebiehaju zmeny.</h1>
+      <p>Stranka bude online hned po aplikacii zmien!</p>
+      </div>
+    </div>
+  </body>
+</html>
