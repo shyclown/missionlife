9e481ae32d4e4f1377ed9494dc0153054722a55f
shyclown <roman.moravcik1@gmail.com>
2017-03-22 19:31:28 +0100

    solved issues with post data

--- system/ng/call.php
+++ system/ng/call.php
@@ -4,18 +4,24 @@ $root = $_SERVER['DOCUMENT_ROOT']."/missionlife";
 require_once($root.'/system/php/define.php');
 require_once($root.'/system/php/class_mysqli.php');
 
+
 if(isset($_GET) && isset($_GET['class']))
 {
   $handle = htmlspecialchars($_GET['class']);
   require_once($root.'/system/php/class_'.$handle.'.php');
+
   $class_name = ucfirst ( $handle ); // uppercase first letter
   $class = new $class_name();
 
-  if($_POST){ $data = $_POST;}
+
+  if(isset($_POST)
+  && isset($_POST['action'])
+  ){ $data = $_POST; }
   else{
     $data = file_get_contents("php://input");
     $data = json_decode($data, true); //array
   }
+
   if(isset($data['action'])){
     if ( method_exists($class, $data['action']) ){
        echo json_encode($class->{$data['action']}($data));
