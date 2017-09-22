ff567af9eb7ad6099de4eaab7bc2c162a15d5cde
shyclown <roman.moravcik1@gmail.com>
2017-09-22 15:29:23 +0200

    update ajax

--- _frontend/app/factory/ajax.js
+++ _frontend/app/factory/ajax.js
@@ -1,12 +1,35 @@
-app.factory('Ajax',function($http){
+app.factory('Ajax', function(){
   return {
     // default functions
     defSuccess : function(response){console.log('success', response.data);},
     defError : function(response){console.log('error', response);},
-    call: function(data, url, completeFn, errorFn){
-      if(!errorFn){ errorFn = this.defError; }
-      $http({ method: 'POST', url: url, data:data, headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
-      .then( completeFn, errorFn );
+    call: function( oData, targetUrl, completeFn, progressFn )
+    {
+
+    const createForm = function(oArray){
+      var oForm = new FormData();
+      for(var oKey in oArray){ oForm.append(oKey,oArray[oKey]); }
+      return oForm;
+    }
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('load',function(){
+      const res = { data: JSON.parse(request.responseText) }
+      if(completeFn){  completeFn(res);  }
+    });
+
+    if(progressFn){
+      request.addEventListener('progress',function(){
+        if(event.lengthComputable){
+          var percent = event.loaded / event.total;
+          progressFn(percent);
+        }
+      });
+    }
+
+    request.open("POST", targetUrl);
+    request.send(createForm(oData));
     }
   }
 });
